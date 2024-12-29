858ac0f4231ff35158f275b74377b7f7533179c0
DenisBisekeev <bisekeevdenis@mail.ru>
2024-12-29 21:43:48 +0300

    загружает файлы сайта

---
+++ index.js
@@ -0,0 +1,22 @@
+function ChangeColor(color) {
+    document.body.style.background = color;
+}
+
+function red() {
+    ChangeColor('red')
+}
+function green() {
+    ChangeColor('green')
+}
+function yellow() {
+    ChangeColor('yellow')
+}
+function grey() {
+    ChangeColor('grey')
+}
+function aqua() {
+    ChangeColor('aqua')
+}
+function white() {
+    ChangeColor('white')
+}
